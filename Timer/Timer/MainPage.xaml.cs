5932aec431be7c4520654f1bb30384da0ce8502f
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-08-11 20:46:23 +0530

    Stopwatch widget now respects the gamebar's requested theme

--- Timer/Timer/MainPage.xaml.cs
+++ Timer/Timer/MainPage.xaml.cs
@@ -1,30 +1,22 @@
 ﻿using Microsoft.Gaming.XboxGameBar;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Timer
 {
-
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
 	public sealed partial class MainPage : Page
 	{
+		XboxGameBarWidget widget = null;
+
 		DispatcherTimer dispatcherTimer;
 		Stopwatch stopwatch;
 
@@ -78,19 +70,31 @@ namespace Timer
 			stopwatch.Reset();
 		}
 
-		XboxGameBarWidget widget = null;
-
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
 			widget = e.Parameter as XboxGameBarWidget;
 
 			if(widget != null)
+			{
 				widget.RequestedOpacityChanged += Widget_RequestedOpacityChanged;
+				widget.RequestedThemeChanged += Widget_RequestedThemeChanged;
+			}
+		}
+
+		private async void Widget_RequestedThemeChanged(XboxGameBarWidget sender, object args)
+		{
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+
+             {
+                 RequestedTheme = widget.RequestedTheme;
+             });
 		}
 
 		private void Widget_RequestedOpacityChanged(XboxGameBarWidget sender, object args)
 		{
 			Background.Opacity = widget.RequestedOpacity;
 		}
+
+		
 	}
 }
