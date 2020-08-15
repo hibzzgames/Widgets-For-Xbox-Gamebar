aeaca66072e0bb953f43c0ac9a47479d7499b70e
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-08-15 21:10:03 +0530

    Project Paused due to API limitations
    Commiting rest of the code just to save progress. Will re-evalueate the program as UWP is sandboxed and doesn't have access to local processes.

--- AltTabber/AltTabber/MainPage.xaml.cs
+++ AltTabber/AltTabber/MainPage.xaml.cs
@@ -1,64 +1,118 @@
 ﻿using Microsoft.Gaming.XboxGameBar;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.System;
+using Windows.System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace AltTabber
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        XboxGameBarWidget widget;
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		XboxGameBarWidget widget;
 
-        public MainPage()
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		#region xbox gamebar code
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			widget = e.Parameter as XboxGameBarWidget;
+
+			if (widget != null)
+			{
+				widget.RequestedOpacityChanged += Widget_RequestedOpacityChanged;
+				widget.RequestedThemeChanged += Widget_RequestedThemeChanged;
+                widget.VisibleChanged += Widget_VisibleChanged;
+			}
+		}
+
+        private async void Widget_VisibleChanged(XboxGameBarWidget sender, object args)
         {
-            this.InitializeComponent();
-        }
+			if (sender.Visible == true)
+			{
+				await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+				{
+					ReloadWindows();
+				});
+			}
+		}
 
-        #region xbox gamebar code
+        private async void Widget_RequestedThemeChanged(XboxGameBarWidget sender, object args)
+		{
+			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+			{
+				RequestedTheme = widget.RequestedTheme;
+			});
+		}
+
+		private void Widget_RequestedOpacityChanged(XboxGameBarWidget sender, object args)
+		{
+			Background.Opacity = widget.RequestedOpacity;
+		}
+
+		#endregion
+		
+		private async void ReloadWindows()
         {
-            widget = e.Parameter as XboxGameBarWidget;
+			bool DiagnosticAccessStatusResult = await GetDiagnosticAccessStatusAsync();
 
-            if (widget != null)
+			if(DiagnosticAccessStatusResult)
             {
-                widget.RequestedOpacityChanged += Widget_RequestedOpacityChanged;
-                widget.RequestedThemeChanged += Widget_RequestedThemeChanged;
+				string s = "";
+
+				var pricesses = Process.GetProcesses();
+
+				var processes = ProcessDiagnosticInfo.GetForProcesses().Where(p => p.IsPackaged).ToArray();
+
+				foreach(var process in processes)
+                {
+					s += process.ExecutableFileName + "\n";
+                }
+				
+				MainText.Text = s;
+				MainText.Visibility = Visibility.Visible;
+			}
+            else
+            {
+				MainText.Visibility = Visibility.Collapsed;
             }
         }
 
-        private async void Widget_RequestedThemeChanged(XboxGameBarWidget sender, object args)
+		private async System.Threading.Tasks.Task<bool> GetDiagnosticAccessStatusAsync()
         {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			DiagnosticAccessStatus diagnosticAccessStatus = await AppDiagnosticInfo.RequestAccessAsync();
+			
+			switch (diagnosticAccessStatus)
+			{
+				case DiagnosticAccessStatus.Allowed:
+					DebugText.Visibility = Visibility.Collapsed;
+					return true;
+				case DiagnosticAccessStatus.Limited:
+					DebugText.Visibility = Visibility.Visible;
+					DebugText.Text = "Diagnostic info is a core feature requirement as we use it to get the list of open applications. " +
+						"We want you to make an informed choice, and we hope you give us the permission. To do so, " +
+						"\n \nGo to Settings > Apps > Apps and Features > AltTabber > Advanced Options, and turn on App Diagnostics. ";
+					return false;
+			}
 
-            {
-                RequestedTheme = widget.RequestedTheme;
-            });
-        }
+			return false;
+		}
 
-        private void Widget_RequestedOpacityChanged(XboxGameBarWidget sender, object args)
-        {
-            Background.Opacity = widget.RequestedOpacity;
-        }
-
-        #endregion
-    }
+	}
 }
